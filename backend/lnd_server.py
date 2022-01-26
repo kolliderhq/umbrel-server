c5bdea408ec507e653ab2d20a5f70a4dcb8c6993
the-kollider <info@kollider.xyz>
2022-01-26 00:25:13 +0000

    seperated hedging and lnd_server

---
+++ backend/lnd_server.py
@@ -0,0 +1,114 @@
+from utils import *
+from lnd_client import LndClient
+from time import sleep
+from threading import Lock
+import json
+import threading
+import lnurl
+from urllib.parse import urlparse, parse_qs
+import requests
+from lnurl_auth import perform_lnurlauth
+import hashlib
+import zmq
+
+SOCKET_ADDRESS = "tcp://*:5556"
+
+SEED_WORD = hashlib.sha256("cheers to you until all enternity and here is my entry ser.".encode("utf-8")).digest()
+
+def lnd_node_server(lnd_client):
+	print("Started lnd node server")
+	context = zmq.Context()
+	socket = context.socket(zmq.REP)
+	socket.bind(SOCKET_ADDRESS)
+	while True:
+		message = ""
+		try:
+			message = socket.recv_json()
+		except Exception as e:
+			print(e)
+			continue
+		if message.get("action") is not None:
+			action = message.get("action")
+			data = message.get("data")
+			if action == "get_node_info":
+				res = lnd_client.get_info()
+				response = {
+					"type": "getNodeInfo",
+					"data": {
+						"identity_pubkey": res.identity_pubkey,
+						"alias": res.alias,
+						"num_active_channels": res.num_active_channels,
+						"num_peers": res.num_peers,
+						"block_height": res.block_height,
+						"block_hash": res.block_hash,
+						"synced_to_chain": res.synced_to_chain,
+						"best_header_timestamp": res.best_header_timestamp,
+						"version": res.version, 
+						"color": res.color,
+						"synced_to_graph": res.synced_to_graph
+					}
+				}
+				socket.send_json(response)
+				continue
+			if action == "create_invoice":
+				message = "kollider"
+				res = lnd_client.add_invoice(data["amount"], message)
+				response = {
+					"type": "createInvoice",
+					"data": {
+						"paymentRequest": res.payment_request
+					}
+				}
+				socket.send_json(response)
+				continue
+			if action == "send_payment":
+				res = lnd_client.send_payment(data["payment_request"])
+				response = {
+					"type": "sendPayment",
+					"data": {
+						"status": "success",
+					}
+				}
+				socket.send_json(response)
+				continue
+			if action == "get_channel_balances":
+				res = lnd_client.get_channel_balances()
+				response = {
+					"type": "getChannelBalances",
+					"data": {
+						"local": res.local_balance.sat,
+						"localMsat": res.local_balance.msat,
+						"remote": res.remote_balance.sat,
+						"remoteMsat": res.remote_balance.msat
+					}
+				}
+				socket.send_json(response)
+				continue
+			if action == "get_wallet_balances":
+				res = lnd_client.get_onchain_balance()
+				response = {
+					"type": "getWalletBalances",
+					"data": {
+						"confirmed_balance": res.confirmed_balance,
+						"total_balance": res.total_balance,
+					}
+				}
+				socket.send_json(response)
+				continue
+			if action == "lnurl_auth":
+				decoded_url = lnurl.decode(data["lnurl"])
+				res = lnd_client.sign_message(SEED_WORD)
+				lnurl_auth_signature = perform_lnurlauth(res.signature, decoded_url)
+				try:
+					_ = requests.get(lnurl_auth_signature)
+					response = {
+						"type": "lnurl_auth",
+						"data": {
+							"status": "success"
+						}
+					}
+					socket.send_json(response)
+				except Exception as e:
+					print(e)
+				continue
+		sleep(0.5)
---
+++ backend/main.py
@@ -0,0 +1,56 @@
+from ast import parse
+from os import get_inheritable
+from random import seed
+from kollider_api_client.ws import KolliderWsClient
+from kollider_api_client.rest import KolliderRestClient
+from utils import *
+from lnd_client import LndClient
+from kollider_msgs import OpenOrder, Position, TradableSymbol, Ticker
+from time import sleep
+from threading import Lock
+import json
+from math import floor
+import uuid
+from pprint import pprint
+import threading
+import lnurl
+from urllib.parse import urlparse, parse_qs
+import requests
+from lnurl_auth import perform_lnurlauth
+import hashlib
+from lnd_server import lnd_node_server
+from lnhedgehog import HedgerEngine
+
+import zmq
+
+def main():
+	settings = None
+	with open('config.json') as a:
+		settings = json.load(a)
+
+	kollider_api_key = settings["kollider"]["api_key"]
+	kollider_url = settings["kollider"]["ws_url"]
+	kollider_passphrase = settings["kollider"]["api_passphrase"]
+	kollider_secret = settings["kollider"]["api_secret"]
+
+	node_url = settings["lnd"]["node_url"]
+	macaroon_path = settings["lnd"]["admin_macaroon_path"]
+	tls_path = settings["lnd"]["tls_path"]
+
+	lnd_client = LndClient(node_url, macaroon_path, tls_path)
+	rn_engine = HedgerEngine(lnd_client)
+
+	lock = Lock()
+
+	if kollider_api_key and kollider_passphrase and kollider_passphrase and kollider_url:
+		print("Starting Hedging Engine")
+		rn_engine.connect(kollider_url, kollider_api_key, kollider_secret, kollider_passphrase)
+		rn_engine.start(settings)
+
+	lnd_node_server_thread = threading.Thread(target=lnd_node_server, daemon=False, args=(lnd_client, ))
+	lnd_node_server_thread.start()
+
+
+if __name__ in "__main__":
+	main()
+
